a4cba9b40539d98d1a41ec4652089630275abbd4
KannegundlaPrerana <preranakannegundla4@gmail.com>
2020-09-29 11:42:34 +0530

    BAH-790 | Venkat,Prerana | 0.93-UT-ManagePrivileges-OnDelete | Add. Test to remove data from formPrivilege when delete icon is clicked

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Dashboard from 'common/Dashboard';
 import FormBuilderContainer from 'form-builder/components/FormBuilderContainer.jsx';
 import FormDetailContainer from 'form-builder/components/FormDetailContainer.jsx';
 import FormTranslationsContainer from 'form-builder/components/FormTranslationsContainer.jsx';
+import FormPrivilegesPreviewContainer from 'form-builder/components/FormPrivilegesPreviewContainer.jsx';
 import FormPrivilegesContainer from 'form-builder/components/FormPrivilegesContainer.jsx';
 import 'bahmni-form-controls';
 const store = createStore(implementerInterface);
